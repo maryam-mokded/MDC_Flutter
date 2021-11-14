7addcd94797640f53855e2ddbd4915713d661a2f
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-11-14 02:10:36 +0100

    Add Shape

--- lib/login.dart
+++ lib/login.dart
@@ -95,6 +95,16 @@ class _LoginPageState extends State<LoginPage> {
                     // TODO: Add buttons (101)
                         TextButton(
                           child: const Text('CANCEL'),
+                          style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all(
+                            Theme.of(context).colorScheme.secondary,
+                          ),
+                          shape: MaterialStateProperty.all(
+                            const BeveledRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                            ),
+                          ),
+                        ),
                           onPressed: () {
                                // TODO: Clear the text fields (101)
                                 _usernameController.clear();
@@ -108,6 +118,11 @@ class _LoginPageState extends State<LoginPage> {
                            // New code
                           style: ButtonStyle(
                             elevation: MaterialStateProperty.all(8.0),
+                            shape: MaterialStateProperty.all(
+                            const BeveledRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                            ),
+                          ),
                           ),
                           onPressed: () {
                                 // TODO: Show the next page (101) 
