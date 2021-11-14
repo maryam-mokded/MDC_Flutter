1d13e9dd0afe99956d6cf712b2590b7548285754
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-11-14 01:30:25 +0100

    Add a top app bar

--- lib/home.dart
+++ lib/home.dart
@@ -9,8 +9,45 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
+          appBar: AppBar(
+        // TODO: Add title (102)
+            title: const Text('SHRINE'),
+        // TODO: Add buttons  (102)
+              leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: () {
+              print('Menu button');
+            },
+          ),
+
+         // TODO: Add trailing buttons (102)
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(
+                    Icons.search,
+                    semanticLabel: 'search',
+                  ),
+                  onPressed: () {
+                    print('Search button');
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.tune,
+                    semanticLabel: 'filter',
+                  ),
+                  onPressed: () {
+                    print('Filter button');
+                  },
+                ),
+              ],
+
+          ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
