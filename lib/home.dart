519cd15f81f301594205abb491245489aa8d6260
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-11-14 02:02:40 +0100

    Modify typography and label styles

--- lib/home.dart
+++ lib/home.dart
@@ -27,7 +27,7 @@ class HomePage extends StatelessWidget {
                 // TODO: Adjust card heights (103)
                 child: Column(
                   // TODO: Center items on the card (103)
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     AspectRatio(
                       aspectRatio: 18 / 11,
@@ -44,20 +44,26 @@ class HomePage extends StatelessWidget {
                         padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
                         child: Column(
                         // TODO: Align labels to the bottom and center (103)
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,  
+                        mainAxisAlignment: MainAxisAlignment.end,
+
                           // TODO: Change innermost Column (103)
                           children: <Widget>[
                           // TODO: Handle overflowing labels (103)
                           Text(
                               product.name,
-                              style: theme.textTheme.headline6,
+                              style: theme.textTheme.button,
+                              softWrap: false,
+                              overflow: TextOverflow.ellipsis,
                               maxLines: 1,
                             ),
-                            const SizedBox(height: 8.0),
+                            const SizedBox(height: 4.0),
                             Text(
                               formatter.format(product.price),
-                              style: theme.textTheme.subtitle2,
+                              style: theme.textTheme.caption,
                             ),
+                              // End new code
+
                           ],
                         ),
                       ),
