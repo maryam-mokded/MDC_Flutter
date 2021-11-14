56f4820cd07c8ef546f6016863616e30e600021f
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-11-14 01:22:40 +0100

    Add buttons

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,9 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+   final _usernameController = TextEditingController();
+   final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +34,7 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Add TextField widgets (101)
             // [Name]
               TextField(
+                controller: _usernameController,
                 decoration: const InputDecoration(
                   filled: true,
                   labelText: 'Username',
@@ -40,6 +44,7 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 12.0),
               // [Password]
               TextField(
+                controller: _passwordController,
                 decoration: const InputDecoration(
                   filled: true,
                   labelText: 'Password',
@@ -48,8 +53,32 @@ class _LoginPageState extends State<LoginPage> {
                 obscureText: true,
               ),
 
-              
+
             // TODO: Add button bar (101)
+             ButtonBar(
+                  // TODO: Add a beveled rectangular border to CANCEL (103)
+                  children: <Widget>[
+                    // TODO: Add buttons (101)
+                        TextButton(
+                          child: const Text('CANCEL'),
+                          onPressed: () {
+                               // TODO: Clear the text fields (101)
+                                _usernameController.clear();
+                                _passwordController.clear();
+                          },
+                        ),
+                        // TODO: Add an elevation to NEXT (103)
+                        // TODO: Add a beveled rectangular border to NEXT (103)
+                        ElevatedButton(
+                          child: const Text('NEXT'),
+                          onPressed: () {
+                                // TODO: Show the next page (101) 
+                                Navigator.pop(context);
+                          },
+                        ),
+                  ],
+                ),
+
           ],
         ),
       ),
